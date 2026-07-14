158ebed8936e927350691c1be0bcbf17010ac187
Mamii868 <marcmotenii@gmail.com>
2026-07-14 12:32:41 -0500

    Add transaction model

--- src/main/java/com/pluralsight/pennywise/models/Transaction.java
+++ src/main/java/com/pluralsight/pennywise/models/Transaction.java
@@ -1,4 +1,89 @@
 package com.pluralsight.pennywise.models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "transaction_id")
+    private int id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "vendor")
+    private String vendor;
+
+    @Column(name = "amount")
+    private double amount;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public Transaction() {
+    }
+
+    public Transaction(int id, String description, String vendor, double amount, int userId, LocalDateTime createdAt) {
+        this.id = id;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+        this.userId = userId;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
