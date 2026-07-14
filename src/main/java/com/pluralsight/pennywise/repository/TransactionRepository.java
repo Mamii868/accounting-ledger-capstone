092f3ab83907af61de332fec52ea591a44ab499e
Mamii868 <marcmotenii@gmail.com>
2026-07-14 12:34:25 -0500

    Add repository files

--- src/main/java/com/pluralsight/pennywise/repository/TransactionRepository.java
+++ src/main/java/com/pluralsight/pennywise/repository/TransactionRepository.java
@@ -1,4 +1,7 @@
 package com.pluralsight.pennywise.repository;
 
-public class TransactionRepository {
+import com.pluralsight.pennywise.models.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 }
--- src/main/java/com/pluralsight/pennywise/repository/UserRepository.java
+++ src/main/java/com/pluralsight/pennywise/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.pluralsight.pennywise.repository;
 
-public class UserRepository {
+import com.pluralsight.pennywise.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
